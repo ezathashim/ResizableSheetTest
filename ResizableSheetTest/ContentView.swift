4e9cf116e16bda11aa7272e92cdc3087c5c16bcd
ezathashim <ezathashim@mac.com>
2026-08-02 14:15:54 -0400

    Initial Commit

---
+++ ResizableSheetTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ResizableSheetTest
+//
+//  Created by Ezat Hashim on 2026-08-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ResizableSheetTest/ResizableSheetTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResizableSheetTestApp.swift
+//  ResizableSheetTest
+//
+//  Created by Ezat Hashim on 2026-08-02.
+//
+
+import SwiftUI
+
+@main
+struct ResizableSheetTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
